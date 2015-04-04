12e8485e299e14f4d8d6f29dbe64b3e95177cca5
Bartłomiej <sakyhank@gmail.com>
2015-04-04 17:54:27 +0000

    Implemented sign up

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :tickets
   end
   
+  resources :users
+  
   
   
   # The priority is based upon order of creation: first created -> highest priority.
